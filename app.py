0bba6aa57d5ff76ea8ede938fa4afaa5ba8473a6
gyanbramha14-ship-it <gyanbramha14@gmail.com>
2026-09-26 16:07:43 +0530

    Update page title in Streamlit configuration

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="PharmaLens 100",
+    page_title="PharmaLens ",
     page_icon="💊",
     layout="wide",
     initial_sidebar_state="expanded"
